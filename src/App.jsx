61600d9528c6ce22ee05437e2beedd262e42d169
Genessy <thibault.scorielle@gmail.com>
2026-05-14 01:01:57 +0200

    Implementation of the architecture

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import Navbar from './components/sections/navbar/Navbar'
+import Hero from './components/sections/hero/Hero'
+import Projects from './components/sections/projects/Projects'
+import Skills from './components/sections/skills/Skills'
+import Contact from './components/sections/contact/Contact'
+import Footer from './components/sections/footer/Footer'
+
 function App() {
 
   return (
     <>
+    <Navbar></Navbar>
+    <Hero></Hero>
+    <Projects></Projects>
+    <Skills></Skills>
+    <Contact></Contact>
+    <Footer></Footer>
     </>
   )
 }
