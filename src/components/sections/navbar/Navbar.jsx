3b17e7ed815c7bc8c160c99f51cde90b1b104d29
Genessy <thibault.scorielle@gmail.com>
2026-05-27 23:38:08 +0200

    Fix: navbar position

--- src/components/sections/navbar/Navbar.jsx
+++ src/components/sections/navbar/Navbar.jsx
@@ -2,9 +2,9 @@ import Logo from '../../../assets/logo.png'
 
 function Navbar() {
     return (
-    <nav className="fixed w-full flex flex-row m-4">
-        <img className='w-15' src={Logo} alt="logo du site" />
-        <div className='flex flex-1 justify-center items-center'>
+    <nav className="w-full flex flex-row mt-4 fixed">
+        <img className='w-15 absolute left-4' src={Logo} alt="logo du site" />
+        <div className='flex justify-center items-center w-full'>
             <ul className='flex gap-4 rounded-full px-6 py-3 text-white backdrop-blur-md bg-white/5 border border-white/10'>
                 <li><a href="">Accueil</a></li>
                 <li><a href="">Projets</a></li>
